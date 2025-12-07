5ef292b81514d9180da385f794216ea71f5aeea1
Shivam Srivastava <shivamsrivastava5095@gmail.com>
2025-12-07 17:11:06 +0530

    Updated Semaphore Class Implementation

--- Scalable_Thread_Management_Library.cpp
+++ Scalable_Thread_Management_Library.cpp
@@ -5,4 +5,28 @@
 #include <mutex>
 #include <condition_variable>
 #include <functional>
+
 using namespace std;
+
+// Semaphore Class Implementation
+class Semaphore {
+private:
+    mutex mtx;
+    condition_variable cv;
+    int count;
+
+public:
+    Semaphore(int c = 0) : count(c) {}
+
+    void acquire() {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [&]() { return count > 0; });
+        count--;
+    }
+
+    void release() {
+        unique_lock<mutex> lock(mtx);
+        count++;
+        cv.notify_one();
+    }
+};
